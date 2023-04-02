42ffd58b973c0827016aa1e8d2a3426edf6042f7
Łukasz Niedzielski <luk20001122@gmail.com>
2023-04-02 22:22:41 +0200

    backend: initial structure

---
+++ src/main/java/pl/GrupaC3/PogoApp/PogoAppApplication.java
@@ -0,0 +1,11 @@
+package main.java.pl.GrupaC3.PogoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogoAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PogoAppApplication.class, args);
+    }
+}
