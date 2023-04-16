1da926294a2d7018f01e12eedd7e2d40e159f98b
Wojciech Pajtel <wojciech.pajtel@student.pk.edu.pl>
2023-04-16 18:33:00 +0200

    add API calls to get weekly weather data for an input location

--- src/main/java/pl/GrupaC3/PogoApp/PogoAppApplication.java
+++ src/main/java/pl/GrupaC3/PogoApp/PogoAppApplication.java
@@ -2,10 +2,18 @@ package pl.GrupaC3.PogoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.GrupaC3.PogoApp.niewiemjaktonazwac.WeatherData;
+import pl.GrupaC3.PogoApp.niewiemjaktonazwac.model.Weather;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PogoAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(PogoAppApplication.class, args);
+
+        ArrayList<Weather> data = WeatherData.weeklyWeatherDataForLocation("Piaski wielkie wolin");
+
+        for(Weather t : data) System.out.println(t.toString());
     }
 }
---
+++ src/main/java/pl/GrupaC3/PogoApp/niewiemjaktonazwac/GPSCoordinates.java
@@ -0,0 +1,30 @@
+package pl.GrupaC3.PogoApp.niewiemjaktonazwac;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.GrupaC3.PogoApp.niewiemjaktonazwac.model.Coordinates;
+
+import java.net.URL;
+
+public class GPSCoordinates {
+    public static Coordinates getCoordinatesForLocation(String location) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Coordinates coords = new Coordinates();
+
+        String link = "https://nominatim.openstreetmap.org/search.php?q="
+                + location
+                + "&format=jsonv2";
+
+        link = link.replace(' ', '+');
+
+        try {
+            JsonNode result = objectMapper.readTree(new URL(link));
+
+            coords.setLatitude(result.get(0).get("lat").asText());
+            coords.setLongitude(result.get(0).get("lon").asText());
+        } catch (Exception e) {
+            return new Coordinates("not", "found");
+        }
+
+        return coords;
+    }
+}
