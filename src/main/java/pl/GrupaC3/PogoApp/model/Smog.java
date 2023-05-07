b16be4df1775b1e18248feecd2ebb7a1772955ba
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-07 19:55:21 +0200

    backend: `Smog` model

--- src/main/java/pl/GrupaC3/PogoApp/model/Smog.java
+++ src/main/java/pl/GrupaC3/PogoApp/model/Smog.java
@@ -1,27 +1,106 @@
 package pl.GrupaC3.PogoApp.model;
 
+import com.fasterxml.jackson.annotation.*;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Smog {
-	private int[] amountUGM = new int[6];
-	private int index = 0;
+	public class IndexLevel {
+		public int id;
+		public String indexLevelName;
+
+		public IndexLevel() {
+			indexLevelName = "Brak Danych";
+		}
+
+		@Override
+		public String toString() {
+			return indexLevelName;
+		}
+	}
+
+	@JsonProperty("id")
+	public int id;
+
+	@JsonProperty("stCalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date stCalcDate;
+
+	@JsonProperty("stIndexLevel")
+	public IndexLevel stIndexLevel;
+
+	@JsonProperty("stSourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date stSourceDataDate;
+
+	@JsonProperty("so2CalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date so2CalcDate;
+
+	@JsonProperty("so2IndexLevel")
+	public IndexLevel so2IndexLevel;
+
+	@JsonProperty("so2SourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date so2SourceDataDate;
+
+	@JsonProperty("no2CalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date no2CalcDate;
+
+	@JsonProperty("no2IndexLevel")
+	public IndexLevel no2IndexLevel;
+
+	@JsonProperty("no2SourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date no2SourceDataDate;
+
+	@JsonProperty("pm10CalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date pm10CalcDate;
+
+	@JsonProperty("pm10IndexLevel")
+	public IndexLevel pm10IndexLevel;
+
+	@JsonProperty("pm10SourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date pm10SourceDataDate;
+
+	@JsonProperty("pm25CalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date pm25CalcDate;
+
+	@JsonProperty("pm25IndexLevel")
+	public IndexLevel pm25IndexLevel;
+
+	@JsonProperty("pm25SourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date pm25SourceDataDate;
+
+	@JsonProperty("o3CalcDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date o3CalcDate;
+
+	@JsonProperty("o3IndexLevel")
+	public IndexLevel o3IndexLevel;
+
+	@JsonProperty("o3SourceDataDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Europe/Warsaw")
+	public Date o3SourceDataDate;
+
+	@JsonProperty("stIndexStatus")
+	public boolean stIndexStatus;
+
+	@JsonProperty("stIndexCrParam")
+	public String stIndexCrParam;
 
 	public Smog() {
-		for (int i = 0; i < 6; i++) {
-			amountUGM[i] = 0;
-		}
+		this.no2IndexLevel = new IndexLevel();
+		this.so2IndexLevel = new IndexLevel();
+		this.o3IndexLevel = new IndexLevel();
+		this.pm25IndexLevel = new IndexLevel();
+		this.pm10IndexLevel = new IndexLevel();
+		this.stIndexLevel = new IndexLevel();
 	}
-
-	public int getAmountUGM3(int value) {
-		return amountUGM[value];
-	}
-
-	public void setAmountUGM3(int value) {
-		if (index == 6) {
-			index = 0;
-		}
-		amountUGM[index] = value;
-		index++;
-	}
-
 }
